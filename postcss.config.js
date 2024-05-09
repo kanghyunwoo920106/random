e9f42ca98c03872ea62500cd35ff9dcf1d1da5cb
hyunwoo <xkaizew12@gmail.com>
2024-05-09 22:31:56 +0900

    first init

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+// postcss.config.js
+
+module.exports = {
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      // 여기에 필요한 다른 PostCSS 플러그인들을 추가할 수 있습니다.
+    ],
+  };
